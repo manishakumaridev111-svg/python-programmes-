9f8dff7516be354391ab99170bd9c343e9ccfc80
manishakumaridev111-svg <manishakumari.dev111@gmail.com>
2026-05-30 18:57:07 +0530

    Update report card.py

--- report card.py
+++ report card.py
@@ -1,1 +1,19 @@
-report_card.py
+# report_card
+name = input("Enter student name: ")
+marks = int(input("Enter marks: "))
+
+if marks >= 90:
+    grade = "A+"
+elif marks >= 75:
+    grade = "A"
+elif marks >= 60:
+    grade = "B"
+elif marks >= 40:
+    grade = "C"
+else:
+    grade = "Fail"
+
+print("\n--- Report Card ---")
+print("Name:", name)
+print("Marks:", marks)
+print("Grade:", grade)
